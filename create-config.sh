3a1d8569247ddcab54c0c5d8e0912d81b1b83912
Thanh Tran <hoangthanh28@gmail.com>
2024-12-14 16:59:06 +0700

    adding init script

---
+++ create-config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ln -s $(pwd)/config/kitty ~/.config/kitty
+ln -s $(pwd)/config/nvim ~/.config/nvim
+ln -s $(pwd)/config/rofi ~/.config/rofi
+ln -s $(pwd)/config/sway ~/.config/sway
+ln -s $(pwd)/config/swaync ~/.config/swaync
+ln -s $(pwd)/config/waybar ~/.config/waybar
+ln -s $(pwd)/config/xdg-desktop-portal ~/.config/xdg-desktop-portal
+ln -s $(pwd)/scripts ~/.scripts
+ln -s $(pwd)/backgrounds ~/.backgrounds
